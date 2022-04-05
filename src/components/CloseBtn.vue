b8324e5ef499f1d9c7e6f0268e2c46fa5e2d8e06
beet <63141491+beetcb@users.noreply.github.com>
2022-04-05 23:16:01 +0800

    complete quzzle 12

---
+++ src/components/CloseBtn.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="close test"></button>
+</template>
+
+<style scoped>
+.test {
+  width: 200px;
+  height: 200px;
+}
+
+.close {
+  border: 1px solid #aaa;
+  border-radius: 50%;
+  position: relative;
+}
+
+.close::before,
+.close::after {
+  content: "";
+  position: absolute;
+  background-color: #aaa;
+  width: 75%;
+  height: 2px;
+}
+
+.close::before {
+  transform: translate(-50%, -50%) rotate(45deg);
+}
+.close::after {
+  transform: translate(-50%, -50%) rotate(-45deg);
+}
+</style>
