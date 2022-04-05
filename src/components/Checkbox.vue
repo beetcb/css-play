1abf10ee6bba90938dd5372d157e4b7ccbc5c709
beet <63141491+beetcb@users.noreply.github.com>
2022-04-05 22:33:20 +0800

    complete quiz 6

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,28 @@
+<template>
+  <label class="my-checkbox">
+    <input type="checkbox" />
+    <span>a checkbox</span>
+  </label>
+</template>
+
+<style scoped>
+/* More advanced guide https://moderncss.dev/pure-css-custom-checkbox-style/ */
+.my-checkbox {
+  display: flex;
+  /* justify-content: center; */
+}
+
+.my-checkbox > input[type="checkbox"] {
+  appearance: none;
+  align-self: center;
+  width: 10px;
+  height: 10px;
+  border: none;
+  border-radius: 50%;
+  background-color: gray;
+}
+
+.my-checkbox > input[type="checkbox"]:checked {
+  background-color: green;
+}
+</style>
