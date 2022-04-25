d0098644b14d6b8a81ffc917c471517f42e48c86
beet <63141491+beetcb@users.noreply.github.com>
2022-04-25 17:52:07 +0800

    clean up

--- src/Play.vue
+++ src/Play.vue
@@ -1,11 +1,21 @@
 <script setup>
 import TurncateText from "./components/TruncateText.vue";
+import CloseBtn from "./components/CloseBtn.vue";
+import MultiCol from "./components/MultiCol.vue";
+import TwitterLayout from "./components/TwitterLayout.vue";
+import Counter from "./components/Counter.vue";
 </script>
 
 <template>
-  <TurncateText />
+  <!-- <TurncateText /> -->
+  <!-- <CloseBtn /> -->
+  <!-- <MultiCol /> -->
+  <!-- <TwitterLayout /> -->
+  <!-- <Counter /> -->
 </template>
 
+<style scoped>
+</style>
 
 
 
